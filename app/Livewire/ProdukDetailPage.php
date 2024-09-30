5d504f58eee5a411be6b763e6e8cb53ce7c8bd45
Rosamdani <rosyamdani2000018114@webmail.uad.ac.id>
2024-10-01 03:04:00 +0700

    edit: produk add to cart

--- app/Livewire/ProdukDetailPage.php
+++ app/Livewire/ProdukDetailPage.php
@@ -2,18 +2,65 @@
 
 namespace App\Livewire;
 
+use App\Livewire\Partials\Navbar;
+use App\Models\Produk;
 use Livewire\Attributes\Lazy;
+use Livewire\Attributes\Title;
 use Livewire\Component;
+use App\Helpers\CartManagement;
 
+
+#[Title('Detail Produk')]
 #[Lazy]
 class ProdukDetailPage extends Component
 {
+    public $produk_id;
+    public $produk_stok;
+
+    public $qty;
+
+    public function mount($id)
+    {
+        $this->produk_id = $id;
+        $this->qty = CartManagement::getProdukStok($this->produk_id);
+        $produk = Produk::where('id', $this->produk_id)->first(['stok']);
+        $this->produk_stok = $produk->stok;
+    }
+
     public function placeholder()
     {
         return view('placeholders.produk-detail');
     }
+
+    public function addToCart($produk_id)
+    {
+        $total_count = CartManagement::addItemToCart($produk_id);
+        $this->dispatch('update-count-cart', total_count: $total_count)->to(Navbar::class);
+        $this->qty = 1;
+    }
+
+    public function increaseQty()
+    {
+        $this->qty >= $this->produk_stok ? $this->qty = $this->produk_stok : $this->qty++;
+        return CartManagement::incrementCartItem($this->produk_id);
+    }
+
+    public function decreaseQty()
+    {
+        $this->qty <= 0 ? $this->qty = 0 : $this->qty--;
+        return CartManagement::decrementCartItem($this->produk_id);
+    }
+    public function getProdukStok()
+    {
+        $produk = Produk::where('id', $this->produk_id)->first(['stok']);
+        return $produk->stok;
+    }
+
     public function render()
     {
-        return view('livewire.produk-detail-page');
+        $produk = Produk::where('id', $this->produk_id)->firstOrFail();
+        return view('livewire.produk-detail-page', [
+            'produk' => $produk
+        ]);
     }
 }
